72fd4422411a0580dde4559c094a0fded294132e
Alex Beardsley <alex.beardsley@isobar.com>
2015-08-25 15:06:56 -0400

    added debug for incoming requests for slack

--- src/main/java/com/integralblue/availability/controller/SlackController.java
+++ src/main/java/com/integralblue/availability/controller/SlackController.java
@@ -3,6 +3,8 @@ package com.integralblue.availability.controller;
 import java.util.Map;
 import java.util.Optional;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -12,17 +14,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.integralblue.availability.model.slack.SlackMessageModelFactory;
 import com.integralblue.availability.model.slack.SlashSlackMessage;
+import com.ullink.slack.simpleslackapi.SlackSession;
+import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory;
 
 @Controller
+@Slf4j
 public class SlackController {
 	@RequestMapping(value="/slack/availability",method=RequestMethod.GET,produces=MediaType.TEXT_PLAIN_VALUE)
 	public ResponseEntity<String> getAvailability(@RequestParam Map<String,String> allRequestParams) {
+		log.debug("Incoming Slack request: " + allRequestParams.toString());
 		Optional<SlashSlackMessage> msg = SlackMessageModelFactory.getSlackMessage(allRequestParams);
 		if (!msg.isPresent())
 			//TODO: thymeleaf automatically intercepts exceptions and tries to redirect to an error template page
 			//		exceptions should be allowed to be thrown in this controller; thymeleaf will probably have to be disabled
 			//		at some point for this controller/url pattern
 			return ResponseEntity.badRequest().body("");
+		
+//		SlackSession session = SlackSessionFactory.createWebSocketSlackSession(authToken);
 		return ResponseEntity.ok("Hello world! " + msg.get());
 	}
 }

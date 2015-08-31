3497328a48d300a51d496ce4b642ee94ce9ebaeb
Craig Andrews <candrews@integralblue.com>
2015-08-31 14:09:43 -0400

    Use Lombok's @Delegate in SlackAvailabilityService to avoid copy/paste

--- src/main/java/com/integralblue/availability/service/impl/SlackAvailabilityService.java
+++ src/main/java/com/integralblue/availability/service/impl/SlackAvailabilityService.java
@@ -1,45 +1,17 @@
 package com.integralblue.availability.service.impl;
 
-import java.util.Date;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import com.integralblue.availability.model.Availability;
-import com.integralblue.availability.model.FreeBusyStatus;
-import com.integralblue.availability.model.Room;
-import com.integralblue.availability.model.RoomList;
-import com.integralblue.availability.properties.ExchangeConnectionProperties;
 import com.integralblue.availability.service.AvailabilityService;
+
+import lombok.experimental.Delegate;
 
 @Service
 public class SlackAvailabilityService implements AvailabilityService {
 	@Autowired
 	@Qualifier("exchangeAvailabilityService")
+	@Delegate
 	private AvailabilityService exchangeAvailabilityService;
-	
-	@Override
-	public Optional<Availability> getAvailability(String emailAddress,
-			Date startDate, Date endDate) {
-		return exchangeAvailabilityService.getAvailability(emailAddress, startDate, endDate);
-	}
-
-	@Override
-	public Set<RoomList> getRoomLists() {
-		return exchangeAvailabilityService.getRoomLists();
-	}
-
-	@Override
-	public Optional<Set<Room>> getRooms(String roomListEmailAddress) {
-		return exchangeAvailabilityService.getRooms(roomListEmailAddress);
-	}
-
-	@Override
-	public Map<Room, FreeBusyStatus> getCurrentRoomsStatus(String roomListEmailAddress) {
-		return exchangeAvailabilityService.getCurrentRoomsStatus(roomListEmailAddress);
-	}
 }

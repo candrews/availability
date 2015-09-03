088a105495230097d524c06908287b89cb615b87
Craig Andrews <candrews@integralblue.com>
2015-09-03 15:44:29 -0400

    Instead of using ConcurrentMapCacheManager to wrap the Guava cache, use GuavaCacheManager which is intended for this purpose

--- src/main/java/com/integralblue/availability/config/CacheConfig.java
+++ src/main/java/com/integralblue/availability/config/CacheConfig.java
@@ -2,11 +2,9 @@ package com.integralblue.availability.config;
 
 import java.util.concurrent.TimeUnit;
 
-import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,12 +15,8 @@ import com.google.common.cache.CacheBuilder;
 public class CacheConfig {
     @Bean
     public CacheManager cacheManager() {
-    	return new ConcurrentMapCacheManager() {
-            @Override
-            protected Cache createConcurrentMapCache(final String name) {
-                return new ConcurrentMapCache(name,
-                    CacheBuilder.newBuilder().expireAfterWrite(5, TimeUnit.MINUTES).build().asMap(), false);
-            }
-        };
+    	final GuavaCacheManager guavaCacheManager = new GuavaCacheManager();
+    	guavaCacheManager.setCacheBuilder(CacheBuilder.newBuilder().expireAfterWrite(5, TimeUnit.MINUTES));
+    	return guavaCacheManager;
     }
 }

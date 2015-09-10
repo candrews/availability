767358118b810c5be9282b70a813e1058fbebe4e
Craig Andrews <candrews@integralblue.com>
2015-09-10 17:44:12 -0400

    Use PBEWITHSHA256AND256BITAES-CBC-BC for encryption instead of PBEWithMD5AndDES

--- src/main/java/com/integralblue/availability/AvailabilityApplication.java
+++ src/main/java/com/integralblue/availability/AvailabilityApplication.java
@@ -1,14 +1,18 @@
 package com.integralblue.availability;
 
+import java.security.Security;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 public class AvailabilityApplication {
+	static {
+		Security.addProvider(new BouncyCastleProvider());
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(AvailabilityApplication.class, args);
